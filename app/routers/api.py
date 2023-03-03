19e0d393e37c9175f819f7152d2d4ed7f70136df
Pandy Knight <andylpk.247@gmail.com>
2023-03-03 11:29:22 -0500

    Logout works, too!

--- app/routers/api.py
+++ app/routers/api.py
@@ -166,6 +166,12 @@ async def post_login(user: UserAccount, response: Response) -> dict():
   return {"message": f"Logged in as {user.username}"}
 
 
+@router.post("/logout", summary="Logs out of the app")
+async def post_login(response: Response, session: str | None = Cookie(default=None)) -> dict():
+  response.set_cookie(key="session", value=session, expires=-1)
+  return {"message": f"Logged out as {session}"}
+
+
 @router.get("/items")
 async def read_items(session: str | None = Cookie(default=None)) -> dict:
   return {"session": session}
