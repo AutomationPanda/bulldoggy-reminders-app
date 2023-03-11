357b721cc47e243f1470ba2b231b883b52e5c5cb
Pandy Knight <andylpk.247@gmail.com>
2023-03-11 00:36:56 -0500

    Add reminder list names from persistence

--- app/routers/reminders.py
+++ app/routers/reminders.py
@@ -6,7 +6,7 @@ This module provides routes for the reminders pages.
 # Imports
 # --------------------------------------------------------------------------------
 
-from app import templates
+from app import templates, table
 from app.utils.auth import get_username_for_page
 
 from fastapi import APIRouter, Depends, Form, Request
@@ -26,7 +26,9 @@ router = APIRouter()
 
 @router.get("/reminders", summary="Logs into the app", response_class=HTMLResponse)
 async def get_reminders(request: Request, username: str = Depends(get_username_for_page)):
-  return templates.TemplateResponse("pages/reminders.html", {'request': request, 'username': username})
+  reminder_lists = table.get_lists(username)
+  context = {'request': request, 'username': username, 'reminder_lists': reminder_lists}
+  return templates.TemplateResponse("pages/reminders.html", context)
 
 
 @router.get("/reminders-frozen", summary="Logs into the app", response_class=HTMLResponse)
