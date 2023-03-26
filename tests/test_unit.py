cdab042fcecd64a877d0dc014185a06a5077741a
Pandy Knight <andylpk.247@gmail.com>
2023-03-26 00:32:08 -0400

    Added a unit test for serialization

---
+++ tests/test_unit.py
@@ -0,0 +1,23 @@
+"""
+This module contains unit tests for the Bulldoggy app.
+"""
+
+# --------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------
+
+from app.utils.auth import serialize_token, deserialize_token
+from testlib.inputs import User
+
+
+# --------------------------------------------------------------------------------
+# Tests
+# --------------------------------------------------------------------------------
+
+def test_token_serialization(user: User):
+  token = serialize_token(user.username)
+  assert token
+  assert isinstance(token, str)
+
+  username = deserialize_token(token)
+  assert username == user.username
